62cb10121fe261e648163d5b84a40d6c8f5ddedb
inzamulhaque <mdihalif@gmail.com>
2022-11-23 11:45:02 +0600

    create user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "please proved user name"],
+    },
+
+    email: {
+      type: String,
+      validate: [validator.isEmail, "Provide a valid Email"],
+      required: [true, "Email address is required"],
+      lowercase: true,
+      unique: [true, "This email already exist"],
+    },
+
+    phone: String,
+
+    role: {
+      type: String,
+      enum: ["student", "mentor", "admin"],
+      default: "student",
+    },
+
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+
+    status: {
+      type: String,
+      default: "inactive",
+      enum: ["active", "inactive", "blocked"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);

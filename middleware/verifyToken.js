4759123d62a36981871c8768fcbd2ddf0c2823f6
inzamulhaque <mdihalif@gmail.com>
2022-12-04 18:55:45 +0600

    fix token problem

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,9 +1,12 @@
 const jwt = require("jsonwebtoken");
-const { promisify } = require("util");
+// const { promisify } = require("util");
 
 const verifyToken = async (req, res, next) => {
   try {
     const token = req.headers?.authorization?.split(" ")?.[1];
+
+    console.log(token);
+
     if (!token) {
       return res.status(401).json({
         success: false,
@@ -11,11 +14,19 @@ const verifyToken = async (req, res, next) => {
       });
     }
 
-    const decoded = await promisify(jwt.verify)(token, process.env.TOKEN_KEY);
+    // const decoded = await promisify(jwt.verify)(token, process.env.TOKEN_KEY);
 
-    req.user = decoded;
+    // req.user = decoded;
 
-    next();
+    // next();
+
+    jwt.verify(token, process.env.TOKEN_KEY, (err, decoded) => {
+      if (err) {
+        return res.status(403).send({ message: "forbidden access", err });
+      }
+      req.user = decoded;
+      next();
+    });
   } catch (error) {
     console.log(error);
     res.status(403).json({
